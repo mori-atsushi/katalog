1f123362ed96a8f134f54984881fd2a0a4839dff
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-06-28 23:09:57 +0900

    Add PreviewTopAppBar

--- katalog/src/main/kotlin/jp/co/cyberagent/katalog/compose/page/PreviewPager.kt
+++ katalog/src/main/kotlin/jp/co/cyberagent/katalog/compose/page/PreviewPager.kt
@@ -1,11 +1,18 @@
 package jp.co.cyberagent.katalog.compose.page
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import jp.co.cyberagent.katalog.compose.KatalogViewModel
 import jp.co.cyberagent.katalog.compose.util.BackPressedEffect
 import jp.co.cyberagent.katalog.compose.widget.Preview
@@ -20,15 +27,40 @@ internal fun PreviewPage(
         viewModel.closePreview()
         true
     }
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colors.background)
     ) {
+        PreviewTopAppBar(
+            name = component.name,
+            onClickClose = viewModel::closePreview
+        )
         Preview(
             modifier = Modifier.fillMaxSize(),
             clickable = true,
             definition = component.definition
         )
     }
+}
+
+@Composable
+private fun PreviewTopAppBar(
+    name: String,
+    onClickClose: () -> Unit
+) {
+    TopAppBar(
+        title = { Text(name) },
+        backgroundColor = MaterialTheme.colors.surface,
+        contentColor = MaterialTheme.colors.onSurface,
+        navigationIcon = {
+            IconButton(onClick = onClickClose) {
+                Icon(
+                    imageVector = Icons.Filled.Close,
+                    contentDescription = "close"
+                )
+            }
+        },
+        elevation = 0.dp
+    )
 }
