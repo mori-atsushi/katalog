60535f9a44ffeff5e1b4403f30d15213b638a600
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-06-28 23:16:11 +0900

    Add animation

--- katalog/src/main/kotlin/jp/co/cyberagent/katalog/compose/widget/ModalVisibility.kt
+++ katalog/src/main/kotlin/jp/co/cyberagent/katalog/compose/widget/ModalVisibility.kt
@@ -4,11 +4,15 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideOut
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
@@ -16,10 +20,13 @@ internal fun <T : Any> ModalVisibility(
     value: T?,
     content: @Composable (T) -> Unit
 ) {
+    val offset: (fullSize: IntSize) -> IntOffset = {
+        IntOffset(0, it.height / 3)
+    }
     AnimatedVisibility(
         visible = value != null,
-        enter = fadeIn(),
-        exit = fadeOut()
+        enter = fadeIn() + slideIn(initialOffset = offset),
+        exit = fadeOut() + slideOut(targetOffset = offset)
     ) {
         var cache by remember {
             mutableStateOf(value)
