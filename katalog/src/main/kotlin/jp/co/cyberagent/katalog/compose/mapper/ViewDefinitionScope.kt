b78c15e58c545852a63dfaeb7cea4c694c9d5773
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-07-11 23:40:23 +0900

    Add custom lifecycle owner

--- katalog/src/main/kotlin/jp/co/cyberagent/katalog/compose/mapper/ViewDefinitionScope.kt
+++ katalog/src/main/kotlin/jp/co/cyberagent/katalog/compose/mapper/ViewDefinitionScope.kt
@@ -5,15 +5,15 @@ import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.LifecycleOwner
+import jp.co.cyberagent.katalog.compose.util.rememberLifecycleOwner
 import jp.co.cyberagent.katalog.dsl.ViewDefinitionScope
 import jp.co.cyberagent.katalog.util.findActivity
 
 @Composable
 internal fun rememberViewDefinitionScope(): ViewDefinitionScope {
     val context = LocalContext.current
-    val lifecycleOwner = LocalLifecycleOwner.current
+    val lifecycleOwner = rememberLifecycleOwner()
     return remember(context, lifecycleOwner) {
         val activity = context.findActivity() ?: error("not found activity")
         ViewDefinitionScopeImpl(
