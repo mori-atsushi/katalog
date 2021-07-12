b03771474f07ce75d47a7e9978b28e71c8f0de92
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-07-12 23:37:02 +0900

    Change Empty UI

--- katalog/src/main/kotlin/jp/co/cyberagent/katalog/compose/widget/Empty.kt
+++ katalog/src/main/kotlin/jp/co/cyberagent/katalog/compose/widget/Empty.kt
@@ -1,10 +1,16 @@
 package jp.co.cyberagent.katalog.compose.widget
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Construction
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -13,13 +19,26 @@ import jp.co.cyberagent.katalog.compose.res.defaultPadding
 
 @Composable
 internal fun Empty() {
-    Text(
+    Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = defaultPadding, vertical = 24.dp),
-        text = "🚧 Empty 🚧",
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.onBackground.copy(alpha = 0.6F),
-        fontSize = 24.sp
-    )
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Construction,
+            contentDescription = null,
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .size(36.dp),
+            tint = MaterialTheme.colors.onBackground.copy(alpha = 0.6F),
+        )
+        Text(
+            text = "No Component Yet",
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colors.onBackground.copy(alpha = 0.4F),
+            fontSize = 16.sp
+        )
+    }
+
 }
