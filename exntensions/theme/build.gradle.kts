598a6ea279027d07e2ed6dbb48f3286543cb431f
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-07-15 00:01:01 +0900

    Add theme ext module

---
+++ exntensions/theme/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    `maven-publish`
+}
+
+android {
+    compileSdk = Constants.compileSdk
+    buildToolsVersion = Constants.buildToolsVersion
+    defaultConfig {
+        minSdk = Constants.minSdk
+        targetSdk = Constants.targetSdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildFeatures {
+        compose = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = freeCompilerArgs + listOf(
+            "-Xexplicit-api=strict",
+            "-Xopt-in=kotlin.RequiresOptIn"
+        )
+    }
+    composeOptions {
+        kotlinCompilerVersion = Versions.composeKotlinCompiler
+        kotlinCompilerExtensionVersion = Versions.Androidx.compose
+    }
+    sourceSets {
+        getByName("main").java.srcDir("src/main/kotlin")
+        getByName("test").java.srcDir("src/test/kotlin")
+        getByName("androidTest").java.srcDir("src/androidTest/kotlin")
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation(project(":katalog"))
+
+    implementation(Deps.Androidx.Compose.ui)
+    implementation(Deps.Androidx.Compose.uiTooling)
+    implementation(Deps.Androidx.Compose.foundation)
+    implementation(Deps.Androidx.Compose.material)
+
+    testImplementation(Deps.Androidx.Test.core)
+    testImplementation(Deps.Androidx.Test.runner)
+    testImplementation(Deps.Androidx.Test.rules)
+    testImplementation(Deps.Androidx.Test.junit)
+    testImplementation(Deps.Androidx.Test.truth)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 rootProject.name = "Katalog"
 include(":katalog")
 include(":katalog-android-view")
+include(":exntensions:theme")
 include(":samples:android-sample")
