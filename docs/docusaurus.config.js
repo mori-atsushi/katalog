4beedcab78479f198923e2cfb279e14f131b1a04
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-07-18 22:59:20 +0900

    Setup config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -3,15 +3,16 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
+  title: 'Katalog',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://cyberagent-zemi.github.io',
+  baseUrl: '/katalog/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'cyberagent-zemi',
+  projectName: 'katalog',
+  trailingSlash: false,
   themeConfig: {
     navbar: {
       title: 'My Site',
