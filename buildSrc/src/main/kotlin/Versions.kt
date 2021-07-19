0b04f1f63515159ef58072ec727c45546edd7460
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-07-19 22:48:51 +0900

    Update libs

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -8,9 +8,9 @@ object Versions {
 
     object Androidx {
         const val ktx = "1.6.0"
-        const val activity = "1.3.0-rc01"
+        const val activity = "1.3.0-rc02"
         const val fragment = "1.3.5"
-        const val compose = "1.0.0-rc01"
+        const val compose = "1.0.0-rc02"
         const val lifecycle = "2.3.1"
         const val lifecycleCompose = "1.0.0-alpha07"
         const val annotation = "1.2.0"
