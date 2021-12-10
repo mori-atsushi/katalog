22fd0e2f364e55c6b5bf1c81d2a3569cf74dcf7d
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-12-10 23:35:29 +0900

    Add findItemById method

--- katalog/src/main/kotlin/jp/co/cyberagent/katalog/domain/Katalog.kt
+++ katalog/src/main/kotlin/jp/co/cyberagent/katalog/domain/Katalog.kt
@@ -4,4 +4,27 @@ internal data class Katalog(
     val title: String,
     val items: List<CatalogItem>,
     val extensions: Extensions
-)
+) {
+    fun findItemById(id: String, ignoreCount: Boolean = false): CatalogItem? {
+        val target = CatalogItemIdentifier.ofOrNull(id) ?: return null
+        val items = target.parents.fold(items) { acc, name ->
+            val result = acc.find {
+                it.name == name
+            }
+            if (result is CatalogItem.Group) {
+                result.items
+            } else {
+                return null
+            }
+        }
+        val result = items.find {
+            it.identifier == target
+        }
+        if (result != null || !ignoreCount) {
+            return result
+        }
+        return items.find {
+            it.identifier.equals(target, true)
+        }
+    }
+}
