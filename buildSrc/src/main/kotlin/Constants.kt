bc0ac235d90ce4ee86e82d217d9f9b70a6525d63
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-04-19 23:07:11 +0900

    Update libraries

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,6 +1,6 @@
 object Constants {
     const val minSdk = 21
     const val targetSdk = 31
-    const val compileSdk = 31
+    const val compileSdk = 33
     const val buildToolsVersion = "30.0.3"
 }
